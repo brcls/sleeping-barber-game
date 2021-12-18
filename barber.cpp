8a28356f9bbedb3e560be978fbd6a9a17f0b9b01
CadavezUSP <fecadavez@usp.br>
2021-12-18 18:19:10 -0300

    null

--- barber.cpp
+++ barber.cpp
@@ -8,34 +8,30 @@ using namespace std;
 #define CORTANDO 1
 #define DESCANSANDO 0
 
-class Barber{
-    private:
-        static int state;
-        static void genereate_energy(){
-            while (state == DESCANSANDO)
-            {
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                energy +=100;
-                cout << energy;
-            }
-        }
-    public:
-        Barber();
-        static int energy;
-        void cut_hair(){
-            if (energy>=200){
-                state = CORTANDO;
-                energy -=200;
-            }
-            else printf("sem energia suficiente");
-        }
-        void sleep(){
+
+Barber::Barber(){
+    state = 0;
+    energy = 1000;
+}
+
+void Barber::sleep(){
             state = DESCANSANDO;
             std::thread recharge(genereate_energy);
             recharge.join();
         }
-};
-Barber::Barber(){
-    state = 0;
-    energy = 1000;
+void Barber::cut_hair(){
+    if (energy>=200){
+                state = CORTANDO;
+                energy -=200;
+            }
+    else printf("sem energia suficiente");
+}
+
+void Barber::genereate_energy(){
+    while (state == DESCANSANDO)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        energy +=100;
+        cout << energy;
+    }
 }
--- barber.h
+++ barber.h
@@ -1,10 +1,10 @@
 class Barber{
     private:
-        int state = 0;
-        void genereate_energy();
+        static int state;
+        static void genereate_energy();
     public:
         Barber();
-        int energy;
+        static int energy;
         void cut_hair();
         void sleep();
 };
