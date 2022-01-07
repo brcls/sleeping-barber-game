6b1b748a71acd3f7191f5da05e1ec1336e9bae3a
JBMolotov <johnnyy.br@gmail.com>
2022-01-07 07:13:00 -0300

    opengl finalizado

--- game.cpp
+++ game.cpp
@@ -16,7 +16,8 @@ static GLubyte *pixeldata;
 #include "barber.h"
 #include "semaphore.h"
 
-#define FILENAME "bobage"
+#define JANELA1 "Barbeiro Sonolento"
+#define JANELA2 "Info do jogo"
 #define QTDIMGS 10
 
 //Estados do barbeiro
@@ -44,26 +45,98 @@ Semaphore customers (0);/*clientes esperando pelo serviço*/
 Semaphore mutex  (1); /*para exclusão mútua*/
 int waiting = 0; /*quantidade de clientes*/
 
+void newPixeldata() {
+    
+    FILE *pfile;
+    
+    //open the certain image
+    if (barber.state == CORTANDO)
+    {
+        if (customers.get_sem() == 0)
+            pfile = fopen("assets/cut0.bmp", "rb");
+        else if (customers.get_sem() == 1)
+            pfile = fopen("assets/cut1.bmp", "rb");
+        else if (customers.get_sem() == 2)
+            pfile = fopen("assets/cut2.bmp", "rb");
+        else if (customers.get_sem() == 3)
+            pfile = fopen("assets/cut3.bmp", "rb");
+        else if (customers.get_sem() == 1)
+            pfile = fopen("assets/cut4.bmp", "rb");
+    }
+    else //if (barber.state == DESCANSANDO || barber.state == EMPEH)
+    {
+        if (customers.get_sem() == 0)
+            pfile = fopen("assets/sleep0.bmp", "rb");
+        else if (customers.get_sem() == 1)
+            pfile = fopen("assets/sleep1.bmp", "rb");
+        else if (customers.get_sem() == 2)
+            pfile = fopen("assets/sleep2.bmp", "rb");
+        else if (customers.get_sem() == 3)
+            pfile = fopen("assets/sleep3.bmp", "rb");
+        else if (customers.get_sem() == 1)
+            pfile = fopen("assets/sleep4.bmp", "rb");
+    }
+
+    if (pfile == 0)
+    {
+        printf("image not found\n");
+        exit(0);
+    }
+
+    //Read image size
+    fseek(pfile, 0x0012, SEEK_SET);
+    fread(&imagewidth, sizeof(imagewidth), 1, pfile);
+    fread(&imageheight, sizeof(imageheight), 1, pfile);
+    
+    //Calculate pixel data length
+    pixellength = imagewidth * 3;
+    while (pixellength % 4 != 0)
+        pixellength++;
+    pixellength *= imageheight;
+    
+    //Read pixel data
+    pixeldata = (GLubyte *)malloc(pixellength);
+    if (pixeldata == 0)
+    {
+        std::cout << "pixel data error";
+        exit(0);
+    }
+        
+    fseek(pfile, 54, SEEK_SET);
+    fread(pixeldata, pixellength, 1, pfile);
+    //Shut down file
+    fclose(pfile);
+}
+
+
 /*Funcao: display
 Descricao: mostra as imagens e outras informações relevantes na tela
 */
-void display(void)
+void displayMain()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    //Draw the image pixel by pixel
-    glDrawPixels(imagewidth, imageheight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixeldata);
-    
-    //---------------------------------
 
+    //Draw the image pixel by pixel
+    glDrawPixels(800, 600, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixeldata);
     
+    glutSwapBuffers();
+}
+
+void displaySecondary()
+{
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     // Mostra a quantidade de energia na tela
-	char barberEnergy[25]={'\0'};
+	char barberEnergy[20]={'\0'};
 	sprintf(barberEnergy, "Energia: %d", barber.energy);// Text
 	glColor3f(1.0,1.0,1.0);// Text color
-	glRasterPos2f(-0.9,-0.9);// Print position
+	glRasterPos2f(0.1,0.2);// Print position
+    // glRasterPos3s(-0.9, -0.9, 1);// Print position
 	// Print each char
-	for(int j=0;j<30;j++){
+	for(int j=0;j<20;j++){
 		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, barberEnergy[j]);
     }
 
@@ -71,80 +144,15 @@ void display(void)
 	char scoreDisplay[25]={'\0'};
 	sprintf(scoreDisplay, "Pontos: %d", score);// Text
 	glColor3f(1.0,1.0,1.0);// Text color
-	glRasterPos2f(0.6,-0.9);// Print position
+	glRasterPos2f(0.1, 0.5);// Print position
 	// Print each char
-	for(int j=0;j<30;j++){
+	for(int j=0;j<25;j++){
 		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, scoreDisplay[j]);
     }
 
     glFlush();
     glutSwapBuffers();
 }
-
-/*Funcao: openAssets
-Descricao: abre as imagens para podermos usar durante o funcionamento do jogo
-*/
-FILE** openAssets (void)
-{
-    //abre os assets
-    FILE **images = (FILE **)malloc(QTDIMGS * sizeof(FILE*));
-    images[0] = fopen("assets/cut1.bmp", "rb");
-    
-    if (images[0] == 0)
-        {
-            std::cout << "images not found";
-            exit(0);
-        }
-
-    return images;
-    // for (int i = 0; i < QTDIMGS; i++)
-    //     if (images[i] == 0)
-    //     {
-    //         cout << "images not found";
-    //         exit(0);
-    //     }
-}
-
-/*Funcao: openAssets
-Descricao: fecha todas as imagens
-*/
-void closeAssets (FILE** images)
-{
-    for (int i = 0; i < 10; i++)
-    {
-        fclose(images[i]);
-    }
-}
-
-/*Funcao: Keyboard
-Descricao: lista qual teclas o jogo ouve e o que fazer com tais teclas
-*/
-void Keyboard(unsigned char key, int x, int y)
-{
-    //ignorar---
-    int w = x + y;
-    w++;
-    //----------
-
-    //X
-    if (key == 'x'){
-	 	std::cout << "(x)" << std::endl;
-	 	glutPostRedisplay();
-    }
-
-    //Z
-    if (key == 'z'){
-	 	std::cout << "(z)" << std::endl;
-	 	glutPostRedisplay();
-    }
-
-    //Q
-    if (key == 'q'){
-	 	std::cout << "carambolas";
-	 	glutPostRedisplay();
-    }
-}
-
 
 /*Funcao: cutting
 Descricao: faz toda a regulação de semaforos e mutex na hora decortar o cabelo
@@ -166,7 +174,7 @@ Descricao: faz toda a regulação de semaforos e mutex na hora de adicionar um n
 */
 void customer ()
 {
-    mtx.lock();
+   mtx.lock();
    if (waiting < CHAIRS)
    {
       waiting = waiting + 1;
@@ -194,7 +202,7 @@ void generate_customer() {
     int max = 5, cTime;
     srand(time(0));
     cTime = (rand() % max) + (5-dificuldade);
-    std::cout << cTime;
+    // std::cout << cTime;
     std::this_thread::sleep_for(std::chrono::seconds(cTime));
     customer();
     generate_customer();
@@ -232,18 +240,53 @@ void get_input(int *exec) {
         get_input(exec);
 }
 
+/*Funcao: Keyboard
+Descricao: lista qual teclas o jogo ouve e o que fazer com tais teclas
+*/
+void Keyboard(unsigned char key, int x, int y)
+{
+    //ignorar---
+    int w = x + y;
+    w++;
+    //----------
+
+    //X
+    if (key == 'x'){
+	 	std::cout << "(x)" << std::endl;
+        if (customers.get_sem() > 0)
+        {
+            cutting();
+        }
+        else
+        {
+            std::cout << "não havia nenhum cliente na loja";
+            barber.state = EMPEH;
+        } 
+    }
+
+    //Z
+    if (key == 'z'){
+	 	barber.sleep();
+    }
+
+    //Q
+    if (key == 'q'){
+	 	exit(0);
+    }
+}
+
 /*Funcao: drenar_energia
 Descricao: função que fica em uma thread executando constantemente drenando energia se o barbeiro esta de pé e nao fazendo nada se ele está em outro estado
 */
 void drenar_energia(int *exec) {
-    std::cout << barber.state << std::endl;
+    // std::cout << barber.state << std::endl;
     if (*exec == PARADO)
         return;
     if (barber.state == EMPEH && *exec ==EXECUTANDO) {
         std::this_thread::sleep_for(std::chrono::seconds(2));
         if (barber.energy > 0){
             barber.energy -=50;
-            std::cout << "sua energia é:" << barber.energy << "\n";
+            // std::cout << "sua energia é:" << barber.energy << "\n";
         } else {
             *exec = PARADO;
             return;
@@ -262,6 +305,11 @@ void initGame(int)
     std::thread generation(generate_customer);
     
     while (executing) {
+        newPixeldata();
+        glutSetWindow(1);
+        glutPostRedisplay();
+        glutSetWindow(2);
+        glutPostRedisplay();
         glutMainLoopEvent();
     }
 
@@ -273,51 +321,33 @@ void initGame(int)
 // função responasavel por toda execução do codigo
 int main(int argc, char *argv[])
 {
-
-    //open a file
-    FILE *pfile = fopen("assets/cut0.bmp", "rb");
-    if (pfile == 0)
-    {
-        printf("image not found\n");
-        exit(0);
-    }
-
-        
-    //Read image size
-    fseek(pfile, 0x0012, SEEK_SET);
-    fread(&imagewidth, sizeof(imagewidth), 1, pfile);
-    fread(&imageheight, sizeof(imageheight), 1, pfile);
-    //Calculate pixel data length
-    pixellength = imagewidth * 3;
-    while (pixellength % 4 != 0)
-        pixellength++;
-    pixellength *= imageheight;
-    //Read pixel data
-    pixeldata = (GLubyte *)malloc(pixellength);
-    if (pixeldata == 0)
-        exit(0);
-    fseek(pfile, 54, SEEK_SET);
-    fread(pixeldata, pixellength, 1, pfile);
-    //Shut down file
-    fclose(pfile);
-
+    newPixeldata();
 
     //Initialize GLUT operation
     glutInit(&argc, argv);   
     
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(100, 50);
     glutInitWindowSize(imagewidth, imageheight);
-    glutCreateWindow(FILENAME);
-    glutDisplayFunc(&display);
+    glutCreateWindow(JANELA1);
+    glutDisplayFunc(&displayMain);
     glutKeyboardFunc(Keyboard);
+    // gluOrtho2D(0, 800, 0, 600);
+    glViewport(0, 0, 800, 600);
+    glOrtho(0, 1, 0, 1, 0, 1);
     // glutMainLoopEvent();
+
+    glutInitWindowPosition(900, 200);
+    glutInitWindowSize(200, 200);
+    glutCreateWindow(JANELA2);
+    glutDisplayFunc(&displaySecondary);
+    glOrtho(0, 1, 0, 1, 0, 1);
+
     initGame(0);
    
     //glutTimerFunc(0, initGame, 0);// Define qual será a função de loop
     // glutMainLoop(); //LOOP DO OPENGL
 
-    
     //-------------------------------------
     free(pixeldata);
     std::cout << "GAME OVER!\n";
